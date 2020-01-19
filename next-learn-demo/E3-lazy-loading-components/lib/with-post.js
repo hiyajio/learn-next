8492887a9ed4aab223cd9f44457f808278cc525e
Juan Sergio Buenviaje <jbuenvia@nd.edu>
2020-01-19 13:37:10 -0500

    Add files via upload

---
+++ next-learn-demo/E3-lazy-loading-components/lib/with-post.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import MyLayout from '../components/MyLayout'
+import marked from 'marked'
+import dynamic from 'next/dynamic'
+
+const Highlight = dynamic(() => import('react-highlight'))
+
+marked.setOptions({
+  gfm: true,
+  tables: true,
+  breaks: true
+})
+
+export default function withPost(options) {
+  return class PostPage extends React.Component {
+    render() {
+      return (
+        <MyLayout>
+          <h1>{options.title}</h1>
+          <div>
+            <Highlight innerHTML>{marked(options.content)}</Highlight>
+          </div>
+        </MyLayout>
+      )
+    }
+  }
+}
